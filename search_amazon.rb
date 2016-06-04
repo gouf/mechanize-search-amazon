1147b1229f5e2a222d0c7e18eac4329344959e53
Gou Furuya <innocent.zero@gmail.com>
2016-06-04 15:38:21 +0900

    Add search function
    used Mechanize

---
+++ search_amazon.rb
@@ -0,0 +1,20 @@
+require 'mechanize'
+require 'uri'
+
+class Mechanize
+  module AmazonSearch
+    class Client
+      AMAZON_JP_URL = 'http://www.amazon.co.jp'.freeze
+      SEARCH_URL = "#{AMAZON_JP_URL}/gp/search?field-keywords=".freeze
+
+      def initialize
+        @mechanize = Mechanize.new
+      end
+
+      def search(key_word)
+        search_result = @mechanize.get("#{SEARCH_URL}#{URI.escape(key_word)}")
+        @search_result = search_result
+      end
+    end
+  end
+end
